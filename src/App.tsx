11a4ad10f4285013b93e1609f752cf3dbff406e4
IchidaU <koo.be.koo@gmail.com>
2025-03-24 23:04:54 +0900

    supabaseからデータ取得を確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import { Button } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { GetAllTodos } from "./lib/todo";
 
 function App() {
+  useEffect(() => {
+    const getAllTodos = async () => {
+      const todosData = await GetAllTodos();
+      console.log(todosData);
+    };
+
+    getAllTodos();
+  }, []);
+
   return (
     <>
       <Button colorScheme="teal">button</Button>
