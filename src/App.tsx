d17a105b538873c2f12a41acad71f586b2abb172
IchidaU <koo.be.koo@gmail.com>
2025-03-28 04:39:39 +0900

    loading画面実装、テスト実装

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,27 @@ import { Todo } from "./domain/todo";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const getAllTodos = async () => {
       const todosData = await GetAllTodos();
+      console.log(todosData);
       setTodos(todosData);
+      setIsLoading(false);
     };
 
     getAllTodos();
   }, []);
 
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
     <>
       <h1 data-testid="title">Todo List</h1>
       <TableContainer>
-        <Table variant="simple">
+        <Table variant="simple" data-testid="table">
           <Thead>
             <Tr>
               <Th>Title</Th>
--- src/__test__/App.spec.tsx
+++ src/__test__/App.spec.tsx
@@ -1,12 +1,38 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 
 import App from "../App";
+import { GetAllTodos } from "../lib/todo";
+import { Todo } from "../domain/todo";
+
+const mockGetAllTodos = jest
+  .fn()
+  .mockResolvedValue([
+    new Todo("1", "title1", false, "2023-01-01"),
+    new Todo("2", "title2", false, "2023-01-01"),
+    new Todo("3", "title3", false, "2023-01-01"),
+    new Todo("4", "title4", false, "2023-01-01"),
+  ]);
+
+jest.mock("../lib/todo", () => {
+  return {
+    GetAllTodos: () => mockGetAllTodos(),
+  };
+});
 
 describe("App", () => {
-  it("タイトルがあること", () => {
+  it("タイトルがあること", async () => {
     render(<App />);
+    await waitFor(() => screen.getByTestId("table"));
     const title = screen.getByTestId("title");
 
     expect(title).toBeInTheDocument();
+  });
+
+  it("TODOが4件表示されること", async () => {
+    render(<App />);
+    await waitFor(() => screen.getByTestId("table"));
+    const todos = screen.getByTestId("table").querySelectorAll("tr");
+
+    expect(todos.length - 1).toBe(4);
   });
 });
